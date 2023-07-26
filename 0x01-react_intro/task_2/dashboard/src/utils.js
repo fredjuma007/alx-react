b73729dc21ce50ba9907d3bb486739e4bddf333c
fredjuma007 <fredjuma007>
2023-07-26 02:29:45 +0200

    adding the notifications and close button

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -8,3 +8,8 @@ export function getFooterCopy(isIndex) {
   return isIndex ? "Holberton School" : "Holberton School main dashboard";
 }
 
+// Function to get the latest notification
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
+}
+
