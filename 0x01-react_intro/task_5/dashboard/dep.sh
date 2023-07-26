cf1445e22f4d61323de1609706317bf40fb1ee9f
fredjuma007 <fredjuma007>
2023-07-26 22:55:27 +0200

    adding installs

---
+++ 0x01-react_intro/task_5/dashboard/dep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+npm install webpack webpack-cli webpack-dev-server --save-dev
+npm install babel-loader @babel/core @babel/preset-env --save-dev
+npm install style-loader css-loader --save-dev
+npm install file-loader --save-dev
+npm install html-webpack-plugin html-loader --save-dev#!/bin/bashnpm install webpack webpack-cli webpack-dev-server --save-dev
+npm install babel-loader @babel/core @babel/preset-env --save-dev
+npm install style-loader css-loader --save-dev
+npm install file-loader --save-dev
+npm install html-webpack-plugin html-loader --save-dev#!/bin/bashnpm install webpack webpack-cli webpack-dev-server --save-dev
+npm install babel-loader @babel/core @babel/preset-env --save-dev
+npm install style-loader css-loader --save-dev
+npm install file-loader --save-dev
+npm install html-webpack-plugin html-loader --save-dev
