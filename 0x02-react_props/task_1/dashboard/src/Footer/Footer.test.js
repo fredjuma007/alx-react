293b8b1280ad639100edaae68379a72a76bbe968
fredjuma007 <fredjuma007>
2023-07-29 03:15:14 +0200

    i hate jest

---
+++ 0x02-react_props/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,16 @@
+// task_1/src/Footer/Footer.test.js
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+
+describe('Footer Component', () => {
+  it('renders without crashing', () => {
+    shallow(<Footer />);
+  });
+
+  it('renders the text "Copyright"', () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.text()).toContain('Copyright');
+  });
+});
