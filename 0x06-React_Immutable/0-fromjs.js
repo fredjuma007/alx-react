5599233530ecc16cc31e6a82f830b49a620c18b0
fredjuma007 <fredjuma007>
2023-08-16 00:57:39 +0200

    fixing the code

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,5 @@
-const { fromJS } = require('immutable');
+const Immutable = require('immutable');
 
-//function that takes an object and returns an immutable map
 function getImmutableObject(object) {
-  return fromJS(object);
+  return Immutable.fromJS(object);
 }
-
-var inputObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-var immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
