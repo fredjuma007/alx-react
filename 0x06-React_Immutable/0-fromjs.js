6a80872ba8c76b0049fb724670c8f8f915a41e8b
fredjuma007 <fredjuma007>
2023-08-16 02:26:36 +0200

    including separate test files

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,3 +3,14 @@ import { fromJS } from 'immutable';
 export default function getImmutableObject(object) {
   return fromJS(object);
 }
+
+const inputObject = {
+    fear: true,
+    smell: -1033575916.9145899,
+    wall: false,
+    thing: -914767132,
+  };
+  
+  const immutableObject = getImmutableObject(inputObject);
+  
+  console.log('Immutable Object:', immutableObject);
